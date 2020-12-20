e7ced5c9999165b5a5b35e304acb0b9b6876751c
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-12-20 19:42:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return (
+      <audio src="" autoPlay></audio>   
+    )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,144 @@
+import { v4 as uuid } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuid(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuid(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuid(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuid(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuid(),
+      active: false,
+    },
+    {
+        name: "Sugarless",
+        cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+        artist: "The Field Tapes, Aviino",
+        id: uuid(),
+        active: false,
+        color: ["#555545", "#4F2319"],
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11243"
+
+    },
+    {
+        name: "Lio",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "Middle School, The Field Tapes",
+        id: uuid(),
+        active: false,
+        color: ["#555545", "#4F2319"],
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11244"
+    },
+    {
+        name: "Harbor",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "Stan Forebee, The Field Tapes, azula, Francis",
+        id: uuid(),
+        active: false,
+        color: ["#555545", "#4F2319"],
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11245"
+    },
+    {
+        name: "Peaches",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "Philanthrope, The Field Tapes",
+        id: uuid(),
+        color: ["#555545", "#4F2319"],
+        active: false,
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11246"
+    },
+    {
+        name: "Loving Someone You Lost",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "The Field Tapes, tender spring, Nuum",
+        id: uuid(),
+        color: ["#555545", "#4F2319"],
+        active: false,
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11247"
+    },
+    {
+        name: "Cloud Carpets",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "The Field Tapes, Ezzy, Wowflower",
+        id: uuid(),
+        color: ["#555545", "#4F2319"],
+        active: false,
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11248"
+    },
+    {
+        name: "Flowers",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        artist: "The Field Tapes, xander.",
+        id: uuid(),
+        color: ["#555545", "#4F2319"],
+        active: false,
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=11249"
+    },
+    {
+        name: "Bliss",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+        artist: "Misha, Jussi Halme",
+        id: uuid(),
+        active: false,
+        color: ["#2A416D", "#F5949F"],
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=9248"
+    }
+  ];
+}
+
+export default chillHop;
+
+
+
+
